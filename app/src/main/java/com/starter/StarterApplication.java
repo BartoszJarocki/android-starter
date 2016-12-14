8a1251972ad50d242090cd1ea35ef301bd542272
Bartosz Jarocki <bartosz.jarocki@smartsoft.io>
2016-12-14 16:32:35 +0100

    Leak cannary added.

--- app/src/main/java/com/starter/StarterApplication.java
+++ app/src/main/java/com/starter/StarterApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.os.StrictMode;
 import android.util.Log;
+import com.squareup.leakcanary.LeakCanary;
 import com.starter.di.AppComponent;
 import com.starter.di.AppModule;
 import com.starter.di.DaggerAppComponent;
@@ -29,7 +30,7 @@ public class StarterApplication extends Application {
 
         initTimber();
         initGraph();
-
+        initLeakCannary();
         enableStrictMode();
     }
 
@@ -50,6 +51,15 @@ public class StarterApplication extends Application {
             .build();
 
         appComponent.inject(this);
+    }
+
+    public void initLeakCannary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
     }
 
     private void enableStrictMode() {
