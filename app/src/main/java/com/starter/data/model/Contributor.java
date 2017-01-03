71391ef54f07692c15f0f26833e9ab2ac1c56bcd
Bartosz Jarocki <bartosz.jarocki@smartsoft.io>
2017-01-03 13:24:57 +0100

    Added ContributorsPresenter unit tests.

--- app/src/main/java/com/starter/data/model/Contributor.java
+++ app/src/main/java/com/starter/data/model/Contributor.java
@@ -16,4 +16,24 @@ public class Contributor {
         this.contributions = contributions;
         this.avatarUrl = avatarUrl;
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Contributor that = (Contributor) o;
+
+        if (contributions != that.contributions) return false;
+        if (login != null ? !login.equals(that.login) : that.login != null) return false;
+        return avatarUrl != null ? avatarUrl.equals(that.avatarUrl) : that.avatarUrl == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = login != null ? login.hashCode() : 0;
+        result = 31 * result + contributions;
+        result = 31 * result + (avatarUrl != null ? avatarUrl.hashCode() : 0);
+        return result;
+    }
 }
---
+++ app/src/test/java/com/starter/ui/contributors/ContributorsPresenterTest.java
@@ -0,0 +1,67 @@
+package com.starter.ui.contributors;
+
+import com.starter.data.model.Contributor;
+import com.starter.network.Api;
+import com.starter.network.ApiManager;
+import com.starter.utils.ThreadConfiguration;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import retrofit2.Response;
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+public class ContributorsPresenterTest {
+
+    private static final String REPO = "owner";
+    private static final String OWNER = "repo";
+    private static final boolean PULL_TO_REFRESH = false;
+
+    @Mock ContributorsView contributorsView;
+    @Mock Api api;
+
+    ApiManager apiManager;
+    ThreadConfiguration threadConfiguration =
+        new ThreadConfiguration(Schedulers.immediate(), Schedulers.immediate());
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+
+        apiManager = new ApiManager(api, threadConfiguration);
+    }
+
+    @Test
+    public void loadContributors() throws Exception {
+        ContributorsPresenter contributorsPresenter = new ContributorsPresenter(apiManager);
+        contributorsPresenter.attachView(contributorsView);
+
+        List<Contributor> contributors = getContributors();
+
+        when(api.contributors(OWNER, REPO)).thenReturn(
+            Observable.just(Response.success(contributors)));
+
+        contributorsPresenter.loadContributors(OWNER, REPO, PULL_TO_REFRESH);
+
+        verify(contributorsView).showProgress(PULL_TO_REFRESH);
+        verify(api).contributors(OWNER, REPO);
+        verify(contributorsView).showContributors(contributors);
+
+        verifyNoMoreInteractions(contributorsView);
+        verifyNoMoreInteractions(api);
+    }
+
+    List<Contributor> getContributors() {
+        List<Contributor> contributors = new ArrayList<>();
+        contributors.add(new Contributor("testuser", 12, "avatar_url"));
+
+        return contributors;
+    }
+}
